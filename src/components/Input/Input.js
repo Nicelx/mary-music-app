9fc5fa1c6073a8184a3c0827083c3cc761791287
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-12-22 22:44:09 +0300

    sign up fetch

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,14 +2,14 @@ import React from 'react'
 import classes from './Input.module.css'
 
 
-export const Input = ({className, placeholder, support}) => {
+export const Input = ({className, placeholder, support, ...props}) => {
 	const clx = [classes.Input]
 	if (className) clx.push(className);
-
+	console.log('props , ', props);
 
 	return (
 		<>
-			<input className = {clx.join(' ')} placeholder = {placeholder} />
+			<input className = {clx.join(' ')} placeholder = {placeholder} onChange={props.onChange}/>
 			{support && support}
 		</>
 			
--- src/containers/SignUp/SignUp.js
+++ src/containers/SignUp/SignUp.js
@@ -6,31 +6,57 @@ import { Input } from "../../components/Input/Input";
 import { useHistory } from "react-router-dom";
 
 export const SignUp = () => {
+	const [login, setLogin] = useState("");
 	const [name, setName] = useState("");
 	const [password, setPassword] = useState("");
 
 	const history = useHistory();
 
 	const onSignUpHandler = async () => {
-		const response = await fetch("http://localhost:8080/auth/signup", {
+		const bodyToSend = JSON.stringify({
+			login,
+			name,
+			password,
+		})
+
+		try {
+			const response = await fetch("http://localhost:8080/auth/signup", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
 			},
-			body: JSON.stringify({
-				login: name,
-				password: password,
-			}),
+			body: bodyToSend,
 		});
+		if (response.status === 201) {
+			history.push('./user')
+		}
 		const data = await response.json();
+		console.log('data', data)
+		}
+		catch(e) {
+			console.log(e);
+			history.push('/signup');
+		}
 	};
+
+	const onNameChangeHandler = (e) => {
+		setName(e.target.value);
+	}
+
+	const onPasswordHandler = e => {
+		setPassword(e.target.value);
+	}
+	const onLoginHandler = e => {
+		setLogin(e.target.value);
+	}
 
 	return (
 		<div className={classes.SignUp}>
 			<h1 className={classes.Welcome}>Welcome</h1>
 			<div className={classes.SignUpForm}>
-				<Input className={classes.Input} placeholder="Name" />
-				<Input className={classes.Input} placeholder="Password" />
+				<Input className={classes.Input} placeholder="Login" onChange ={onLoginHandler} />
+				<Input className={classes.Input} placeholder="Name" onChange ={onNameChangeHandler} />
+				<Input className={classes.Input} placeholder="Password" onChange ={onPasswordHandler}/>
 				<Button className={classes.Button} onClick={onSignUpHandler}>
 					Sign Up
 				</Button>
